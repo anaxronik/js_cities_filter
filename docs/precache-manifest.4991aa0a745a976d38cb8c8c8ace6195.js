786f680d7cfc2e42eed80cc236d1dd27f3e90e4a
Sergey Komarov <anaxronik@gmail.com>
2020-07-09 20:19:52 +0500

    create git hub page

---
+++ docs/precache-manifest.4991aa0a745a976d38cb8c8c8ace6195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91e87e0eb78c2af22bfcff8ee093376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df5e704bd1e782771ca4",
+    "url": "/static/css/main.95ca911e.chunk.css"
+  },
+  {
+    "revision": "ee5dcd6ecf9716769339",
+    "url": "/static/js/2.49835ff0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49835ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5e704bd1e782771ca4",
+    "url": "/static/js/main.3ee8fe80.chunk.js"
+  },
+  {
+    "revision": "35153aa40318992fdd90",
+    "url": "/static/js/runtime-main.58db4531.js"
+  }
+]);
