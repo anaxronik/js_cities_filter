0d3c21db380143599a235d47532d654f6a30bd61
Sergey Komarov <anaxronik@gmail.com>
2020-07-09 20:39:59 +0500

    Updates

---
+++ precache-manifest.0c2e1b48dab2302ae194ffe229e8a28e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe18bdb95e266b5a9db1e74c7aaf3d39",
+    "url": "/js_cities_filter/index.html"
+  },
+  {
+    "revision": "df5e704bd1e782771ca4",
+    "url": "/js_cities_filter/static/css/main.95ca911e.chunk.css"
+  },
+  {
+    "revision": "ee5dcd6ecf9716769339",
+    "url": "/js_cities_filter/static/js/2.49835ff0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js_cities_filter/static/js/2.49835ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5e704bd1e782771ca4",
+    "url": "/js_cities_filter/static/js/main.3ee8fe80.chunk.js"
+  },
+  {
+    "revision": "2808ae85c5f99db0e0ad",
+    "url": "/js_cities_filter/static/js/runtime-main.1d83cab8.js"
+  }
+]);
